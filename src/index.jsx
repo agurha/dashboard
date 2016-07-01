6954261d26b5bbe9acc455a4852dc435c147c356
Marcin Mazurek <marcin.mazurek@which.co.uk>
2016-07-01 12:59:41 +0100

    Initial commit

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const Application = () => <h1>Dashboard</h1>;
+
+ReactDOM.render(<Application />, document.getElementById("app"));
