8f99c4f2a3e6c5579c4b5e6b80e4804fcd34d6f6
Marcin Mazurek <marcin.mazurek@which.co.uk>
2016-07-24 11:11:26 +0100

    Add additional error handling to getExchangeRate function

--- client/services/get-exchange-rate.js
+++ client/services/get-exchange-rate.js
@@ -1,6 +1,11 @@
 export default async function getExchangeRate(baseCurrency, targetCurrency) {
   try {
     const rateRequest = await fetch(`/api/exchange-rates/${baseCurrency}-${targetCurrency}`);
+    
+    if (!rateRequest.ok) {
+      throw new Error('Failed to fetch the exchange rate.');
+    }
+
     return await rateRequest.text();
   } catch (e) {
     throw new Error('Failed to fetch the exchange rate.');
