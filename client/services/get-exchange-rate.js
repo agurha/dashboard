c66617a704dcc0a3f1286b79de0ad17f8e57f5a5
Marcin Mazurek <marcin.mazurek@which.co.uk>
2016-07-24 09:55:29 +0100

    Deployment to Heroku - combine API and front-end servers into one server for sake of simplicity

--- client/services/get-exchange-rate.js
+++ client/services/get-exchange-rate.js
@@ -1,6 +1,6 @@
 export default async function getExchangeRate(baseCurrency, targetCurrency) {
   try {
-    const rateRequest = await fetch(`http://localhost:3001/exchange-rates/${baseCurrency}-${targetCurrency}`);
+    const rateRequest = await fetch(`/api/exchange-rates/${baseCurrency}-${targetCurrency}`);
     return await rateRequest.text();
   } catch (e) {
     throw new Error('Failed to fetch the exchange rate.');
--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-const express = require('express');
+// polyfills
+import 'isomorphic-fetch';
+import 'babel-polyfill';
+
+import express from 'express';
+import getExchangeRate from 'api/services/get-exchange-rate';
+
 const app = express();
 const port = (process.env.PORT || 3000);
 
@@ -6,4 +12,16 @@ app.use('/', express.static('public'));
 
 app.listen(port, function () {
   console.log('Server running on port ' + port + '.');
+});
+
+app.get('/api/exchange-rates/:baseCurrency-:targetCurrency', async (request, response) => {
+  const params = request.params;
+
+  try {
+    const rate = await getExchangeRate(params.baseCurrency, params.targetCurrency);
+    response.send(rate);
+  } catch (e) {
+    console.warn(e);
+    response.status(500).end('Failed to get the exchange rate.')
+  }
 });
