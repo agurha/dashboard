d1ec725c152bbf42620d36dd4b05d34372755ede
Marcin Mazurek <marcin.mazurek@which.co.uk>
2016-07-24 11:22:13 +0100

    Add a simple authentication to the application

---
+++ README.md
@@ -0,0 +1,7 @@
+## Environment variables
+
+| Name | Description |
+|------|-------------|
+| `APPLICATION_AUTH_ENABLED` | Whether basic HTTP auth is enabled for the application. Set to `true` to enable authentication. |
+| `APPLICATION_LOGIN` | Login for authorized user. |
+| `APPLICATION_PASSWORD` | Password for authorized user. |
--- server.js
+++ server.js
@@ -4,15 +4,23 @@ import 'babel-polyfill';
 
 import express from 'express';
 import getExchangeRate from 'api/services/get-exchange-rate';
+import auth from 'http-auth';
 
 const app = express();
-const port = (process.env.PORT || 3000);
+
+const config = process.env;
+
+if (config.APPLICATION_AUTH_ENABLED === 'true') {
+  const authentication = auth.basic({
+      realm: "Marcin's dashboard"
+    }, (login, password, callback) => {
+      callback(login === config.APPLICATION_LOGIN && password === config.APPLICATION_PASSWORD);
+    }
+  );
+  app.use(auth.connect(authentication));
+}
 
 app.use('/', express.static('public'));
-
-app.listen(port, function () {
-  console.log('Server running on port ' + port + '.');
-});
 
 app.get('/api/exchange-rates/:baseCurrency-:targetCurrency', async (request, response) => {
   const params = request.params;
@@ -24,4 +32,9 @@ app.get('/api/exchange-rates/:baseCurrency-:targetCurrency', async (request, res
     console.warn(e);
     response.status(500).end('Failed to get the exchange rate.')
   }
+});
+
+const port = (config.PORT || 3000);
+app.listen(port, function () {
+  console.log('Application running on port ' + port + '.');
 });
