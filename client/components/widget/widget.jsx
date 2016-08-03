b35158ff397ad142be92a259264cca9fb064a8f7
Marcin Mazurek <marcin.mazurek@which.co.uk>
2016-08-03 20:21:27 +0100

    Add margin between widgets

--- client/components/widget/widget.jsx
+++ client/components/widget/widget.jsx
@@ -1,15 +1,18 @@
 import React, { Component, PropTypes } from 'react';
 import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
+import styles from './widget.scss';
 
 export default class Widget extends Component {
   render() {
     const { children, title, ...otherProps } = this.props;
 
     return (
-      <Card {...otherProps}>
-        {title ? <CardTitle title={title} /> : null}
-        {children}
-      </Card>
+      <div className={styles.container}>
+        <Card {...otherProps}>
+          {title ? <CardTitle title={title} /> : null}
+          {children}
+        </Card>
+      </div>
     );
   }
 }
