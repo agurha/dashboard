7076fb5c35b880b298d1d2d89b678aea5138a575
Marcin Mazurek <marcin.mazurek@which.co.uk>
2016-08-04 19:55:21 +0100

    Fix wrong date format in the date and time widget

--- client/services/get-date-time.js
+++ client/services/get-date-time.js
@@ -7,6 +7,6 @@ export default function getDateTime() {
     hour: date('HH'),
     minute: date('mm'),
     dayOfWeek: date('dddd'),
-    date: date('MM.DD.YYYY')
+    date: date('DD.MM.YYYY')
   };
 }
